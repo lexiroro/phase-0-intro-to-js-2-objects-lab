1ff5a68b5d3d99eeabac2721566cc5a10e4784ce
Alexandra Romano <alex.romano80@gmail.com>
2022-11-21 19:02:50 -0700

    commit tree b8cd272a7cd66102f4c21870c33b764a2c9cd08d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+name: "Lexi",
+streetAddress: "1301 Scottsdale Rd",
+};
+
+
+function updateEmployeeWithKeyAndValue (obj,key,value) {
+ const newObj  = {...obj};
+ newObj[key] = value;
+ return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+return obj;
+}
+
+function deleteFromEmployeeByKey(obj,key) {
+    const newObj = {...obj};
+    delete newObj[key];
+    return newObj;
+}
